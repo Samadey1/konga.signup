624d0a2fbdc8e41e49cb3851ea8ac872d2b49e68
Samuel <adeyemoademolasamuel@yahoo.ca>
2023-12-27 23:50:52 +0100

    welcome

---
+++ cypress/e2e/signup.cy.js
@@ -0,0 +1,22 @@
+import {signup} from '../fixtures/selectors';
+
+describe('smoke testing', ()=> {
+  beforeEach( ()=> {
+    cy.visit('https://jumia.com.ng')
+  })
+
+  it("should be able to signup", ()=> {
+
+  })
+
+})
+
+    
+
+
+
+
+
+
+
+
